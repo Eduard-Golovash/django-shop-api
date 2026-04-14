8a54252119834151d81394f488d57745855612c7
vash-ed <vash-ed@ya.ru>
2026-04-14 18:23:54 +0300

    Корректировка зависимостей. Тестовое вью и урл

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterUserView
+from .views import RegisterUserView, TestAuthView
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('test-auth/', TestAuthView.as_view(), name='test_auth'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,7 +1,22 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 from .serializers import UserSerializer
 from .models import User
+
 
 class RegisterUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class TestAuthView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "message": "OK",
+            "user": request.user.username
+        })
